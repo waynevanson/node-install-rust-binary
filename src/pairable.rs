16c3a06ad0d5efeb7b1ef2c8b2fad381f6758588
Wayne Van Son <waynevanson@gmail.com>
2022-12-23 10:52:03 +1100

    tests(pair): add coverage

--- src/pairable.rs
+++ src/pairable.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, PartialEq, PartialOrd)]
 pub struct Pair<A, B>(A, B);
 
 impl<A, B> Pair<A, B> {
@@ -25,5 +26,46 @@ impl<A, B> From<Pair<A, B>> for (A, B) {
 impl<A, B> From<(A, B)> for Pair<A, B> {
     fn from(tuple: (A, B)) -> Self {
         Self(tuple.0, tuple.1)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn should_convert_from_tuple_to_pair() {
+        let first = "first";
+        let second = "second";
+        let result = Pair::from((first, second));
+        let expected = Pair(first, second);
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn should_convert_from_pair_to_tuple() {
+        let first = "first";
+        let second = "second";
+        let result: (_, _) = Pair(first, second).into();
+        let expected = (first, second);
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn should_map_the_first_argument_only() {
+        let first = 4;
+        let second = 9;
+        let result = Pair(first, second).map_first(|number| number * number);
+        let expected = Pair(16, second);
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn should_map_the_second_argument_only() {
+        let first = 4;
+        let second = 9;
+        let result = Pair(first, second).fmap_second(|number| number * number);
+        let expected = Pair(first, 81);
+        assert_eq!(result, expected);
     }
 }
