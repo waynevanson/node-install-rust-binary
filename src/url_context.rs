75a7ecdf5ee1d3851c7e35d0f88b73f16bf660ed
Wayne Van Son <waynevanson@gmail.com>
2022-12-23 11:27:25 +1100

    tests(url_context): add coverage

--- src/url_context.rs
+++ src/url_context.rs
@@ -20,3 +20,58 @@ impl UrlContext {
         tt.render(name, self).unwrap()
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use serde_json::json;
+    use std::convert::TryFrom;
+
+    #[test]
+    fn should_serialise_to_json() {
+        let name = "name".to_string();
+        let bin = "bin".to_string();
+        let version = Version::try_from("1.0.0").unwrap();
+        let triple = "triple".to_string();
+
+        let result = serde_json::to_string(&UrlContext {
+            bin: bin.to_string(),
+            name: name.to_string(),
+            triple: triple.to_string(),
+            version: version.clone(),
+        })
+        .unwrap();
+
+        let expected = json!({
+            "name": name,
+            "version": version,
+            "bin": bin,
+            "triple": triple
+        })
+        .to_string();
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn should_subsitute_context_values() {
+        let name = "name".to_string();
+        let bin = "bin".to_string();
+        let version = Version::try_from("1.0.0").unwrap();
+        let triple = "triple".to_string();
+
+        let pattern = "{name}{bin}{triple}-{version}";
+
+        let result = UrlContext {
+            bin: bin.to_string(),
+            name: name.to_string(),
+            triple: triple.to_string(),
+            version: version.clone(),
+        }
+        .subsitute(pattern);
+
+        let expected = "namebintriple-1.0.0";
+
+        assert_eq!(result, expected);
+    }
+}
